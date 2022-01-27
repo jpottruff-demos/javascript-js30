e5a8ea04ef33cd2502d602b20accda859763e4e5
Jeff Pottruff <jeffpottruff@gmail.com>
2022-01-26 20:55:12 -0500

    Lesson 7 - Adds js

--- 07 - Array Cardio 2/script.js
+++ 07 - Array Cardio 2/script.js
@@ -13,14 +13,37 @@ const comments = [
     { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
-  // Some and Every Checks
-  // Array.prototype.some() // is at least one person 19 or older?
-  // Array.prototype.every() // is everyone 19 or older?
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
 
-  // Array.prototype.find()
-  // Find is like filter, but instead returns just the one you are looking for
-  // find the comment with the ID of 823423
+const oneOver19 = people.some(person => {
+    const curYear = (new Date()).getFullYear();
+    return curYear - person.year >= 19;
+});
+console.log(oneOver19);
 
-  // Array.prototype.findIndex()
-  // Find the comment with this ID
-  // delete the comment with the ID of 823423
+const allOver19 = people.every(person => {
+    const curYear = (new Date()).getFullYear();
+    return curYear - person.year >= 19;
+});
+
+console.log(allOver19);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const searchId = 823423;
+
+const found = comments.find(comment => comment.id === searchId);
+console.log(found);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+const matchingIndex = comments.findIndex(comment => comment.id === searchId);
+console.log(matchingIndex)
+
+comments.splice(matchingIndex, 1)
+console.log(comments)
