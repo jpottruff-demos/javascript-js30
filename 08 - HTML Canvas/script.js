9b2c4cf767d458b69fbae103d13a1be2dae510fc
Jeff Pottruff <jeffpottruff@gmail.com>
2022-01-29 10:46:27 -0500

    Lesson 8 - adds basic lines on canvas

--- 08 - HTML Canvas/script.js
+++ 08 - HTML Canvas/script.js
@@ -1,1 +1,36 @@
-console.log('hey there')
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+// Canvas Setup
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Context Setup
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+    if (!isDrawing) return;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    // lastX = e.offsetX;
+    // lastY = e.offsetY;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
