6ade25f19bf6652104ab7c07371fda65baf66fa6
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-10 19:13:13 -0400

    Lesson 18 - tally using reduce

--- 18 - Tally times with reduce/script.js
+++ 18 - Tally times with reduce/script.js
@@ -1,1 +1,19 @@
-console.log('hey there')
+const timeNodes = document.querySelectorAll("[data-time]");
+
+const totalSeconds = Array.from(timeNodes)
+  .map((node) => node.dataset.time)
+  .map((timeCode) => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, seconds) => {
+    return total + seconds;
+  }, 0);
+
+let secondsLeft = totalSeconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
