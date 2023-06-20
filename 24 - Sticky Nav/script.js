3f70252f07c35ec438e63a37cfceb3fc15c7025d
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-19 22:38:10 -0400

    Lesson 24 - add sticky header

--- 24 - Sticky Nav/script.js
+++ 24 - Sticky Nav/script.js
@@ -1,1 +1,16 @@
-console.log('hey there')
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    // offset the body to avoid the 'jump' when nav goes to 'fixed' position
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    // pop class on the body
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
