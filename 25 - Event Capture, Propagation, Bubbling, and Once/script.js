35cba768b12465303c23e6ce916c0ea1042cb4cd
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-19 23:09:54 -0400

    Lesson 25 - adds js

--- 25 - Event Capture, Propagation, Bubbling, and Once/script.js
+++ 25 - Event Capture, Propagation, Bubbling, and Once/script.js
@@ -1,7 +1,21 @@
 const divs = document.querySelectorAll("div");
+const button = document.querySelector("button");
 
 function logText(e) {
   console.log(this.classList.value);
+  // * prevent bubbling
+  //   e.stopPropagation();
 }
 
-divs.forEach((div) => div.addEventListener("click", logText));
+divs.forEach((div) =>
+  div.addEventListener("click", logText, {
+    // * NOTE: see how this works with stopPropagation above
+    // capture: true, // * run the functions on the way down instead of waiting for them to bubble up; default = false
+  })
+);
+
+button.addEventListener("click", logText, {
+  once: true, // * unbinds listener after first click
+});
+// * just to illustrate that clicks are being triggered here too
+// document.body.addEventListener("click", logText);
