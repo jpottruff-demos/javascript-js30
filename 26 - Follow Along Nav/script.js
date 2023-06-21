29dac87fe34a529c35a45d482c8539ffc79f9a30
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-21 08:58:54 -0400

    Lesson 26 - add/remove dropdown items on hover

--- 26 - Follow Along Nav/script.js
+++ 26 - Follow Along Nav/script.js
@@ -1,1 +1,20 @@
-console.log('hey there')
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
+
+function handleEnter() {
+  this.classList.add("trigger-enter");
+  setTimeout(() => this.classList.add("trigger-enter-active"), 150); // see CSS note
+}
+
+function handleLeave() {
+  this.classList.remove("trigger-enter", "trigger-enter-active");
+}
+
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);
