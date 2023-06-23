2660250c8138c510a3cf430ddc1b258a9a785661
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-23 10:55:19 -0400

    Lesson 29 - initial countdown logic

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,1 +1,27 @@
-console.log('hey there')
+let countdown;
+
+function timer(seconds) {
+  // * NOTE: this causes issues in some browsers with scrolling, tab-offs, etc
+  //   setInterval(() => {
+  //     seconds--;
+  //   }, 1000);
+
+  const now = Date.now(); // * NOTE: in ms
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  console.log({ minutes, remainderSeconds });
+}
