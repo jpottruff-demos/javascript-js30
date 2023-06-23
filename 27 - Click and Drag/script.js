e003bfbaa82f5761cefaa22f875a4543cb078089
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-23 10:05:23 -0400

    Lesson 27 - adds basic code for slide effect

--- 27 - Click and Drag/script.js
+++ 27 - Click and Drag/script.js
@@ -1,1 +1,34 @@
-console.log('hey there')
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  // Capture initial vairables
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  // Set default state
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  // Set default state
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  // Do slide effect
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  //   const walk = x - startX;
+  const walk = (x - startX) * 3; // * gives more of an effect
+  slider.scrollLeft = scrollLeft - walk; // * including initial scrollLeft avoids some jumpiness
+});
