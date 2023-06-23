7bee8ec182d307964554505812d075ae46f3d4a5
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-23 10:55:42 -0400

    Lesson 29 - remove commented code

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,11 +1,6 @@
 let countdown;
 
 function timer(seconds) {
-  // * NOTE: this causes issues in some browsers with scrolling, tab-offs, etc
-  //   setInterval(() => {
-  //     seconds--;
-  //   }, 1000);
-
   const now = Date.now(); // * NOTE: in ms
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
