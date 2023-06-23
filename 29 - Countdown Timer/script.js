e6360eb4add827e1fe59718eb97297e8084407f9
Jeff Pottruff <jeffpottruff@gmail.com>
2023-06-23 11:14:55 -0400

    Lesson 29 - display end time

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,9 +1,11 @@
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 let countdown;
 
 function timer(seconds) {
   const now = Date.now(); // * NOTE: in ms
   const then = now + seconds * 1000;
+  displayEndTime(then);
   displayTimeLeft(seconds);
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,4 +26,14 @@ function displayTimeLeft(seconds) {
   }${remainderSeconds}`;
   document.title = displayTime;
   timerDisplay.textContent = displayTime;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const min = end.getMinutes();
+  //   endTime.textContent = `Be back @ ${hour}:${min}`; // 24hr clock
+  endTime.textContent = `Be back @ ${hour > 12 ? hour - 12 : hour}:${
+    min < 10 ? "0" : ""
+  }${min}`; // 12hr clock
 }
